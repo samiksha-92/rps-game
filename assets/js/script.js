c6a3935e0a2792c85c5d2060f85976a4643fc6f4
Samiksha <mailtosamiksha.s@gmail.com>
2022-08-19 11:41:08 +0000

    cached DOM Elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+
+/**Caching the DOM Elements for future use **/
+let userScore_span = document.getElementById("user-score");
+let compScore_span = document.getElementById("comp-score");
+let scoreBoard_div = document.querySelector(".score-board");
+let result_div = document.querySelector("result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
