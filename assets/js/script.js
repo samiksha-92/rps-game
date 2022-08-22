c6a9b4587777587bace400391924489c4f2b4c30
Samiksha <mailtosamiksha.s@gmail.com>
2022-08-22 08:56:49 +0000

    fix : add onclick events to img divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,60 @@ let result_div = document.querySelector("result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+
+/**Getting Computer choice randomly from choices array */
+function getComputerChoice() {
+    let choices = ['r', 'p', 's'];
+    let randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+
+}
+
+
+/** comparing the userChoice and compChoice */
+function runGame(userChoice) {
+    let computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("user wins");
+            /**win(); */
+            break;
+        case "ps":
+        case "rp":
+        case "sr":
+            console.log("user loses");
+            /**  lose(); */
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("its a draw");
+            /**  draw(); */
+
+    }
+
+}
+
+/**Getting userChoice and passing the value to runGame function*/
+function main() {
+
+    /**Adding event listeners to r,p,s divs */
+
+    rock_div.addEventListener("click", function () {
+        console.log("clicked");
+        runGame("r");
+    })
+
+    paper_div.addEventListener("click", function () {
+        console.log("clicked");
+        runGame("p");
+    })
+
+    scissors_div.addEventListener("click", function () {
+        console.log("clicked");
+        runGame("s");
+    })
+}
